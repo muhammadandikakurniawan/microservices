f0e4098e137ade17b56ce5d6a154f796977fa119
muhammadandikakurniawan <muhammadandikakurniawan24@gmail.com>
2019-11-02 09:23:04 +0700

    first service : java with spring boot

---
+++ m1_java_springboot/src/main/java/com/JavaService/m1_java_springboot/M1JavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.JavaService.m1_java_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M1JavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M1JavaSpringbootApplication.class, args);
+	}
+
+}
---
+++ m1_java_springboot/src/test/java/com/JavaService/m1_java_springboot/M1JavaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaService.m1_java_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1JavaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
