d1c24e0a1f3e0683c8a5f4f550367467617a524c
muhammadandikakurniawan <muhammadandikakurniawan24@gmail.com>
2019-11-28 11:57:29 +0700

    add service send email

---
+++ m2_java_spring_boot/src/main/java/m2_java_spring_boot/m2_java_spring_boot/M2JavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package m2_java_spring_boot.m2_java_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2JavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2JavaSpringBootApplication.class, args);
+	}
+
+}
---
+++ m2_java_spring_boot/src/test/java/m2_java_spring_boot/m2_java_spring_boot/M2JavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package m2_java_spring_boot.m2_java_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2JavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
